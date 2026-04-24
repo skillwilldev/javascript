0d286e0d8f44569fc11b02c0248f8f6381dd961d
Melik <skillwillstudy@gmail.com>
2026-04-24 21:04:27 +0400

    task-2

--- homeworks/task-1/script.js
+++ homeworks/task-1/script.js
@@ -1,8 +1,24 @@
-let massMark = 90;
-let heightMark =1.9;
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height ** 2 = mass / (height * height). (mass in kg and height in meter).
 
-let massJohn = 80;
-let heightJohn = 1.75;
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs using the formula (you can even implement both versions)
+3. Create a boolean variable 'markHigherBMI' containing information about whether Mark has a higher BMI than John.
+
+TEST DATA 1: Marks weights 78 kg and is 1.69 m tall. John weights 92 kg and is 1.95 m tall.
+TEST DATA 2: Marks weights 95 kg and is 1.88 m tall. John weights 85 kg and is 1.76 m tall.
+
+GOOD LUCK 😀
+*/
+///////////////////////////////////////////////////////////////
+
+
+
+let massMark = 78;
+let heightMark = 1.69;
+
+let massJohn = 92;
+let heightJohn = 1.95;
 
 const BMIMark = massMark / heightMark ** 2;
 const BMIJohn = massJohn / (heightJohn * heightJohn);
